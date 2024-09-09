a59f5a911136f4d7cf79bbf3cdcfa49c7a25909b
rombru <rom.bruyere@gmail.com>
2024-09-09 21:45:56 +0200

    feat: adapt build to target 2020 IDEA versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "be.bruyere.romain"
-version = "2024.0.0"
+version = "2020.0.0"
 
 repositories {
   mavenCentral()
@@ -16,7 +16,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2024.1") // Lowest target IDE Version
+  version.set("2020.2") // Lowest target IDE Version
   type.set("IC") // Target IDE Platform
 
   plugins.set(listOf("java", "com.intellij.platform.images"))
@@ -28,18 +28,18 @@ dependencies {
 
 tasks {
   withType<JavaCompile> {
-    sourceCompatibility = "17"
-    targetCompatibility = "17"
+    sourceCompatibility = "11"
+    targetCompatibility = "11"
   }
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     compilerOptions {
-      jvmTarget.set(JvmTarget.JVM_17)
+      jvmTarget.set(JvmTarget.JVM_11)
     }
   }
 
   patchPluginXml {
-    sinceBuild.set("241")
-    untilBuild.set("")
+    sinceBuild.set("202")
+    untilBuild.set("203.*")
   }
 
   signPlugin {

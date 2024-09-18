455f88009fba35043d1d3ec6e204b98d076f5f09
rombru <rom.bruyere@gmail.com>
2024-09-18 21:26:27 +0200

    feat: update change notes

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ tasks {
   patchPluginXml {
     sinceBuild.set("231")
     untilBuild.set("233.*")
-    changeNotes.set("Changes the dialog window for it to be modal")
+    changeNotes.set("Reproject JTS geometries based on their SRID")
   }
 
   signPlugin {

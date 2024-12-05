1e989a51574bcf0782f567f849dc7b9dfa6e8342
rombru <rom.bruyere@gmail.com>
2024-12-05 21:50:29 +0100

    fix: dispose frame before disposing browser

--- src/main/kotlin/be/bruyere/romain/geometryviewer/GeometryViewerFrame.kt
+++ src/main/kotlin/be/bruyere/romain/geometryviewer/GeometryViewerFrame.kt
@@ -156,9 +156,10 @@ object GeometryViewerFrame {
     }
 
     fun closeBrowser() {
+        jFrame?.dispose()
+        jFrame = null
         jbCefBrowser?.dispose()
         jbCefBrowser = null
-        jFrame = null
     }
 
 
